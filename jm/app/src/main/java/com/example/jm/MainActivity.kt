f32ae2a19c9a95d94a1f6856f74a2d1481ff1a2e
Jang Muyeong <136911104+wkdan@users.noreply.github.com>
2023-09-23 04:21:25 +0900

    week1 test

--- jm/app/src/main/java/com/example/jm/MainActivity.kt
+++ jm/app/src/main/java/com/example/jm/MainActivity.kt
@@ -8,6 +8,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_home)
     }
 }
