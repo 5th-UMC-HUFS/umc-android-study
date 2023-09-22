3744186f230738062cc01d5ee1ac2b3923b44ef1
KangMinji <1117minsky@hufs.ac.kr>
2023-09-23 05:27:03 +0900

    feat: week1 mission

--- mindy/week1/build.gradle.kts
+++ mindy/week1/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //bottomnavigation
+    implementation ("androidx.navigation:navigation-fragment:2.3.2")
+    implementation ("androidx.navigation:navigation-ui:2.3.2")
 }
