c59cf2772ea5312125e6ebfc5ff1cb7761b7a918
정다교 <hyunn0121@duksung.ac.kr>
2023-09-23 15:10:46 +0900

    feat : week1 Layout Mission

---
+++ coda/app/src/main/java/com/UMC/study/LockerFragment.kt
@@ -0,0 +1,23 @@
+package com.UMC.study
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.UMC.study.databinding.FragmentLockerBinding
+
+class LockerFragment : Fragment() {
+
+    lateinit var binding: FragmentLockerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
