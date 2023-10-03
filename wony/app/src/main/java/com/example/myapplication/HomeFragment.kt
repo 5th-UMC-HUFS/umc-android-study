3e13a6563feec8997e6a7792fe42febcab451f43
wony <sally09290@naver.com>
2023-10-03 20:10:34 +0900

    final week1

---
+++ wony/app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    //mainFragment 연결
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
--- wony/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ wony/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,61 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) //첫 시작 화면
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation() //바텀 네비게이션을 클릭 후 다시 기존 화면으로 돌아왔을 때 유지
 
     }
+
+    private fun initBottomNavigation(){
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss() //호출 시 Fragment의 상태 : 추가되거나 제거됨
+        //commit() : 실행하기
+
+        binding.mainBnv.setOnItemSelectedListener{ item -> //mainBnv가 눌렸을 때
+            when (item.itemId) {
+
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment()) //main_frm에 HomeFragment를 넣어라!
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.lookFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LookFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.lockerFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LockerFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+
+    }
+
+
 }
