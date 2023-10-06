01ea366b70e9e35957e165df5471966f5fb85490
dusskdus <skdusb0007@naver.com>
2023-10-06 23:12:13 +0900

    week3 : basic mission

--- kei/app/src/main/java/com/dusskdus/study/HomeFragment.kt
+++ kei/app/src/main/java/com/dusskdus/study/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.dusskdus.study.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,10 +20,18 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        binding.todayReleaseAlbum1ImgIv.setOnClickListener{
+        binding.homeAlbumImgIv1.setOnClickListener{
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss() //main activity의 레이아웃을 어디서 변경하는지 기록
 
         }
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+
         return binding.root
     }
 }
